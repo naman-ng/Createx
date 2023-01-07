31f591d2dca8fb5dbc2f86d6e707208f143805a9
Naman <naman21171@iiitd.ac.in>
2023-01-08 00:05:05 +0530

    hardhat: done and dusted

--- hardhat-tut/scripts/deploy.js
+++ hardhat-tut/scripts/deploy.js
@@ -1,14 +1,21 @@
 const { ethers } = require('hardhat');
 require('dotenv').config({ path: '.env' });
-const {} = require('../constants');
+// const {} = require('../constants');
 
 async function main() {
-  const sampleContract = await ethers.getContractFactory('Sample');
+  const [deployer] = await hre.ethers.getSigners();
+  const accountBalance = await deployer.getBalance();
 
-  const deployedSampleContract = await sampleContract.deploy();
-  await deployedSampleContract.deployed();
+  console.log('Deploying contract with account: ', deployer.address);
+  console.log('Account balance: ', accountBalance.toString());
 
-  console.log('Sample Contract Address:', deployedSampleContract.address);
+  const contractFactory = await ethers.getContractFactory('VideoMarketplace');
+  // no params in constructor
+  const contract = await contractFactory.deploy();
+
+  await contract.deployed();
+
+  console.log('contract VideoMarketplace deployed to address: ', contract.address);
 }
 
 // Call the main function and catch if there is any error
