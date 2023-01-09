0cb55051db52c71e986ba9a70ebe62e8de52ecc9
Naman <naman21171@iiitd.ac.in>
2023-01-09 12:21:13 +0530

    update

--- frontend/pages/Creator/uploadContent.jsx
+++ frontend/pages/Creator/uploadContent.jsx
@@ -1,79 +1,80 @@
-// import { useCreateAsset } from '@livepeer/react';
-// import { useCallback, useState } from 'react';
-// import { useDropzone } from 'react-dropzone';
+import BigNumber from 'bignumber.js';
+import { useContext, useState } from 'react';
+import { MainContext } from '../../context';
+import { WebBundlr } from '@bundlr-network/client';
 
+export default function uploadContent() {
+  const [file, setFile] = useState();
+  const [image, setImage] = useState();
+  const [URI, setURI] = useState();
+  const [amount, setAmount] = useState();
+  const { bundlrInstance, initialiseBundlr, balance, fetchBalance } = useContext(MainContext);
 
-// export default function uploadVideo() {
-//   const [video, setVideo] = useState<File | undefined>();
-//   const {
-//     mutate: createAsset,
-//     data: asset,
-//     status,
-//     progress,
-//     error,
-//   } = useCreateAsset(
-//     video
-//       ? {
-//           sources: [{ name: video.name, file: video }] as const,
-//         }
-//       : null,
-//   );
- 
-//   const onDrop = useCallback(async (acceptedFiles: File[]) => {
-//     if (acceptedFiles && acceptedFiles.length > 0 && acceptedFiles?.[0]) {
-//       setVideo(acceptedFiles[0]);
-//     }
-//   }, []);
- 
-//   const { getRootProps, getInputProps } = useDropzone({
-//     accept: {
-//       'video/*': ['*.mp4'],
-//     },
-//     maxFiles: 1,
-//     onDrop,
-//   });
- 
-//   const progressFormatted = useMemo(
-//     () =>
-//       progress?.[0].phase === 'failed'
-//         ? 'Failed to process video.'
-//         : progress?.[0].phase === 'waiting'
-//         ? 'Waiting'
-//         : progress?.[0].phase === 'uploading'
-//         ? `Uploading: ${Math.round(progress?.[0]?.progress * 100)}%`
-//         : progress?.[0].phase === 'processing'
-//         ? `Processing: ${Math.round(progress?.[0].progress * 100)}%`
-//         : null,
-//     [progress],
-//   );
- 
-//   return (
-//     <>
-//       <Box {...getRootProps()}>
-//         <Box as="input" {...getInputProps()} />
-//         <Box as="p">
-//           <Text>Drag and drop or browse files</Text>
-//         </Box>
-//       </Box>
- 
-//       {createError?.message && <Text>{createError.message}</Text>}
- 
-//       {video ? (
-//         <Badge>{video.name}</Badge>
-//       ) : (
-//         <Text>Select a video file to upload.</Text>
-//       )}
-//       {progressFormatted && <Text>{progressFormatted}</Text>}
- 
-//       <Button
-//         onClick={() => {
-//           createAsset?.();
-//         }}
-//         size="2"
-//         disabled={!createAsset || createStatus === 'loading'}
-//       >
-//         Upload
-//       </Button>
-//     </>
-//   );
-// };
+  async function initialize() {
+    initialiseBundlr();
+  }
+
+  function onFileChange(e) {
+    const file = e.target.files[0];
+    if (file) {
+      const image = URL.createObjectURL(file);
+      setImage(image);
+      let reader = new FileReader();
+      reader.onload = function () {
+        if (reader.result) {
+          setFile(Buffer.from(reader.result));
+        }
+      };
+      reader.readAsArrayBuffer(file);
+    }
+  }
+
+  async function uploadFile() {
+    const tx = await bundlrInstance.uploader.upload(file, [{ name: 'Content-Type', value: 'image/png' }]);
+    console.log('tx: ', tx.data);
+    setURI(`http://arweave.net/${tx.data.id}`);
+    // "ar://0dO1AWy6u8JabFjEHQQBwrT3dEaD_LHm_wYoGP4VSYI"
+    // http://arweave.net/2arYGOhXEuNdurgTE72gqruCNL7twudM7K6EWVjCsfA
+  }
+
+  async function fundWallet() {
+    if (!amount) return;
+    const amountParsed = parseInput(amount);
+    let response = await bundlrInstance.fund(amountParsed);
+    console.log('Wallet funded: ', response);
+    fetchBalance();
+  }
+
+  function parseInput(input) {
+    const conv = new BigNumber(input).multipliedBy(bundlrInstance.currencyConfig.base[1]);
+    if (conv.isLessThan(1)) {
+      console.log('error: value too small');
+      return;
+    } else {
+      return conv;
+    }
+  }
+
+  return (
+    <div style={containerStyle}>
+      {!balance && <button onClick={initialize}>Initialize</button>}
+      {balance && (
+        <div>
+          <h3>Balance: {balance}</h3>
+          <div style={{ padding: '20px 0px' }}>
+            <input onChange={(e) => setAmount(e.target.value)} />
+            <button onClick={fundWallet}>Fund Wallet</button>
+          </div>
+          <input type="file" onChange={onFileChange} />
+          <button onClick={uploadFile}>Upload File</button>
+          {image && <img src={image} style={{ width: '200px' }} />}
+          {URI && <a href={URI}>{URI}</a>}
+        </div>
+      )}
+    </div>
+  );
+}
+
+const containerStyle = {
+  padding: '100px 20px',
+};
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,10 @@
 import '../styles/globals.css';
-
 import { LivepeerConfig, createReactClient, studioProvider } from '@livepeer/react';
+import { WebBundlr } from '@bundlr-network/client';
+import { MainContext } from '../context';
+import { useState, useRef } from 'react';
+import { providers, utils } from 'ethers';
+
 const livepeerClient = createReactClient({
   provider: studioProvider({
     apiKey: process.env.NEXT_PUBLIC_STUDIO_API_KEY,
@@ -8,9 +12,46 @@ const livepeerClient = createReactClient({
 });
 
 export default function App({ Component, pageProps }) {
+  const [bundlrInstance, setBundlrInstance] = useState();
+  const [balance, setBalance] = useState();
+  const bundlrRef = useRef();
+
+  async function initialiseBundlr() {
+    await window.ethereum.enable();
+
+    const provider = new providers.Web3Provider(window.ethereum);
+    await provider._ready();
+
+    const bundlr = new WebBundlr('https://devnet.bundlr.network', 'matic', provider, {
+      providerUrl: 'https://thrumming-quiet-yard.matic-testnet.discover.quiknode.pro/e8d17c21d6f86cdc291e6c8fa44a6868c51ee863/',
+    });
+    await bundlr.ready();
+
+    setBundlrInstance(bundlr);
+    bundlrRef.current = bundlr;
+    fetchBalance();
+  }
+
+  async function fetchBalance() {
+    const bal = await bundlrRef.current.getLoadedBalance();
+    console.log('bal: ', utils.formatEther(bal.toString()));
+    setBalance(utils.formatEther(bal.toString()));
+  }
+
   return (
-    <LivepeerConfig client={livepeerClient}>
-      <Component {...pageProps} />
-    </LivepeerConfig>
+    <div>
+      <MainContext.Provider
+        value={{
+          initialiseBundlr,
+          bundlrInstance,
+          balance,
+          fetchBalance,
+        }}
+      >
+        <LivepeerConfig client={livepeerClient}>
+          <Component {...pageProps} />
+        </LivepeerConfig>
+      </MainContext.Provider>
+    </div>
   );
 }
